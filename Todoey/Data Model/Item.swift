560a7cf9232f72f32c58a64efee9df74d63cdb0d
Wilson Hew <wilsonhew@wilsons-macbook-air.local>
2018-12-27 22:32:59 +0800

    Commit to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Wilson Hew on 27/12/2018.
+//  Copyright © 2018 Wilson Hew. All rights reserved.
+//
+
+import Foundation
+
+class Item
+{
+    var title: String = ""
+    var done: Bool = false
+}
